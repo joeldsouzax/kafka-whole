c783a0a82bcc2fc25333f511d3b8bfe81d3b6a6e
Joel DSouza <joeldsouzax@gmail.com>
2024-12-19 16:08:01 +0100

    chore: added sandwich domain

---
+++ sandwich-recipes/src/domain/sandwich.rs
@@ -0,0 +1,12 @@
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const SANDWICH_ID: &str = "sand-id";
+    const SANDWICH_NAME: &str = "Hot dog";
+
+    #[test]
+    fn should_create_the_expected_sandwich() {
+        let ingredients = vec!["Wurst".to_string(), "Ketchup".to_string()];
+    }
+}
