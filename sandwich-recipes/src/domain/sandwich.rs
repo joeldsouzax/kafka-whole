b63ea16b2ab8500b8aaa8c46fa4c147b28eafd5d
Joel DSouza <joeldsouzax@gmail.com>
2024-12-19 16:14:21 +0100

    chore: added serde and sandwich model

--- sandwich-recipes/src/domain/sandwich.rs
+++ sandwich-recipes/src/domain/sandwich.rs
@@ -1,3 +1,22 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Sandwich {
+    id: String,
+    name: String,
+    ingredients: Vec<String>,
+}
+
+impl Sandwich {
+    pub fn new(id: String, name: String, ingredients: Vec<String>) -> Self {
+        Self {
+            id,
+            name,
+            ingredients,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -8,5 +27,22 @@ mod tests {
     #[test]
     fn should_create_the_expected_sandwich() {
         let ingredients = vec!["Wurst".to_string(), "Ketchup".to_string()];
+
+        let hot_dog = Sandwich::new(
+            SANDWICH_ID.to_string(),
+            SANDWICH_NAME.to_string(),
+            ingredients.clone(),
+            SandwichType::Meat,
+        )
+        .unwrap();
+
+        assert_eq!(hot_dog.id().value().as_ref().unwrap(), SANDWICH_ID);
+        assert_eq!(hot_dog.name.value(), SANDWICH_NAME);
+
+        assert_eq!(ingredients.len(), hot_dog.ingredients.value().len());
+
+        for (i, exp_ingr) in ingredients.iter().enumerate() {
+            assert_eq!(exp_ingr, &hot_dog.ingredients.value()[i]);
+        }
     }
 }
