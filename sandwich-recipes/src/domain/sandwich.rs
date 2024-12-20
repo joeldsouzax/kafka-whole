2e7ab31864c220d5a544f9f16a1400bfde183f25
Joel DSouza <joeldsouzax@gmail.com>
2024-12-20 12:37:47 +0100

    chore: added Sandwich ingredient type

--- sandwich-recipes/src/domain/sandwich.rs
+++ sandwich-recipes/src/domain/sandwich.rs
@@ -45,6 +45,29 @@ impl TryFrom<String> for SandwichName {
     }
 }
 
+// sandwich ingredients
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct SandwichIngredients(Vec<String>);
+
+impl SandwichIngredients {
+    pub fn value(&self) -> &Vec<String> {
+        &self.0
+    }
+}
+
+impl TryFrom<Vec<String>> for SandwichIngredients {
+    type Error = &'static str;
+
+    fn try_from(ingredients: Vec<String>) -> Result<Self, Self::Error> {
+        if ingredients.is_empty() {
+            Err("Any sandwich must have at least one ingredient")
+        } else {
+            Ok(Self(ingredients))
+        }
+    }
+}
+
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum SandwichType {
     Meat,
