1c1b9c15343458e2bd5dabe0ec923d2d8337357b
Joel DSouza <joeldsouzax@gmail.com>
2024-12-20 11:05:57 +0100

    chore: added test for failing sandwich creation

--- sandwich-recipes/src/domain/sandwich.rs
+++ sandwich-recipes/src/domain/sandwich.rs
@@ -32,17 +32,34 @@ mod tests {
             SANDWICH_ID.to_string(),
             SANDWICH_NAME.to_string(),
             ingredients.clone(),
-            SandwichType::Meat,
-        )
-        .unwrap();
+        );
 
-        assert_eq!(hot_dog.id().value().as_ref().unwrap(), SANDWICH_ID);
-        assert_eq!(hot_dog.name.value(), SANDWICH_NAME);
-
-        assert_eq!(ingredients.len(), hot_dog.ingredients.value().len());
-
+        assert_eq!(&hot_dog.id, SANDWICH_ID);
+        assert_eq!(&hot_dog.name, SANDWICH_NAME);
+        assert_eq!(ingredients.len(), hot_dog.ingredients.len());
         for (i, exp_ingr) in ingredients.iter().enumerate() {
-            assert_eq!(exp_ingr, &hot_dog.ingredients.value()[i]);
+            assert_eq!(exp_ingr, &hot_dog.ingredients[i]);
         }
+    }
+
+    #[test]
+    fn should_fail_without_a_name_or_ingredients() {
+        // idk what error type we gonna get right now
+        let err_sandwich: Result<Sandwich, Box<dyn Error>> = Sandwich::new(
+            "".to_string(),
+            "".to_string(),
+            vec!["Wurst".to_string(), "Ketchup".to_string()],
+        );
+        assert_eq!(err_sandwich.is_err(), true);
+        assert_eq!(err_sandwich.unwrap_err(), "Any Sandwich must have a name");
+
+        let err_sandwich: Result<Sandwich, Box<dyn Error>> =
+            Sandwich::new(SANDWICH_ID.to_string(), SANDWICH_NAME.to_string(), vec![]);
+
+        assert_eq!(err_sandwich.is_err(), true);
+        assert_eq!(
+            err_sandwich.unwrap_err(),
+            "Any sandwich must have at least an ingredient"
+        );
     }
 }
