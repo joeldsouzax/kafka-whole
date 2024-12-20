be6f1017b985949a7bd290a2b27f3d7e0917de51
Joel DSouza <joeldsouzax@gmail.com>
2024-12-20 21:02:23 +0100

    chore: formatted create sandwich use case

--- sandwich-recipes/src/domain/create_sandwich.rs
+++ sandwich-recipes/src/domain/create_sandwich.rs
@@ -12,7 +12,6 @@ mod tests {
         let ingredients = vec!["Wurst", "Ketchup"];
         let sandwich = create_sandwich("Hot dog", &ingredients, &SandwichType::Meat).unwrap();
         assert_eq!(sandwich.name().value(), "Hot dog");
-
         assert_eq!(sandwich.ingredients().value().len(), ingredients.len());
         for (i, exp_ingr) in sandwich.ingredients().value.iter().enumerate() {
             assert_eq!(exp_ingr, &ingredients[i]);
