2e21d88fae5cb02cb2a80e8959aca9ed974b78dd
Joel DSouza <joeldsouzax@gmail.com>
2024-12-20 11:11:50 +0100

    chore: made the test pass

--- sandwich-recipes/src/domain/sandwich.rs
+++ sandwich-recipes/src/domain/sandwich.rs
@@ -8,12 +8,20 @@ pub struct Sandwich {
 }
 
 impl Sandwich {
-    pub fn new(id: String, name: String, ingredients: Vec<String>) -> Self {
-        Self {
+    pub fn new(id: String, name: String, ingredients: Vec<String>) -> Result<Self, String> {
+        if name.is_empty() {
+            return Err("Any sandwich must have a name".to_string());
+        }
+
+        if ingredients.is_empty() {
+            return Err("Any sandwich must have at least an ingredient".to_string());
+        }
+
+        Ok(Self {
             id,
             name,
             ingredients,
-        }
+        })
     }
 }
 
@@ -32,7 +40,8 @@ mod tests {
             SANDWICH_ID.to_string(),
             SANDWICH_NAME.to_string(),
             ingredients.clone(),
-        );
+        )
+        .unwrap();
 
         assert_eq!(&hot_dog.id, SANDWICH_ID);
         assert_eq!(&hot_dog.name, SANDWICH_NAME);
@@ -45,15 +54,15 @@ mod tests {
     #[test]
     fn should_fail_without_a_name_or_ingredients() {
         // idk what error type we gonna get right now
-        let err_sandwich: Result<Sandwich, Box<dyn Error>> = Sandwich::new(
+        let err_sandwich = Sandwich::new(
             "".to_string(),
             "".to_string(),
             vec!["Wurst".to_string(), "Ketchup".to_string()],
         );
         assert_eq!(err_sandwich.is_err(), true);
-        assert_eq!(err_sandwich.unwrap_err(), "Any Sandwich must have a name");
+        assert_eq!(err_sandwich.unwrap_err(), "Any sandwich must have a name");
 
-        let err_sandwich: Result<Sandwich, Box<dyn Error>> =
+        let err_sandwich =
             Sandwich::new(SANDWICH_ID.to_string(), SANDWICH_NAME.to_string(), vec![]);
 
         assert_eq!(err_sandwich.is_err(), true);
