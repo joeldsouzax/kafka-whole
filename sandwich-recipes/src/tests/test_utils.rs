726e69e258db1a7ac9a0dc3ac6a07e9cbc3db837
Joel DSouza <joeldsouzax@gmail.com>
2024-12-22 22:20:07 +0100

    chore: added test utils helper functions

--- sandwich-recipes/src/tests/test_utils.rs
+++ sandwich-recipes/src/tests/test_utils.rs
@@ -6,4 +6,21 @@ pub mod shared {
     pub const SANDWICH_NAME: &str = "Hot dog";
     pub const SANDWICH_TYPE: SandwichType = SandwichType::Meat;
     pub const CHEESEBURGER_NAME: &str = "Cheeseburger";
+
+    pub fn assert_on_sandwich(expected: Sandwich, actual: &Sandwich, assert_on_id: bool) {
+        if assert_on_id {
+            assert_eq!(
+                actual.id().value().as_ref().unwrap(),
+                expected.id().value().as_ref().unwrap()
+            );
+        }
+
+        assert_eq!(actual.name().value(), expected.name().value());
+    }
+
+    pub fn assert_on_ingredients(
+        expected_ingredients: &Vec<String>,
+        actual_ingredients: &Vec<String>,
+    ) {
+    }
 }
