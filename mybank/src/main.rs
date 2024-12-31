60fedc1d090e757ea1da6f6b4073e637f5b6a9ca
Joel DSouza <joeldsouzax@gmail.com>
2024-12-31 12:25:43 +0100

    feat(mybank): added commands and events

--- mybank/src/main.rs
+++ mybank/src/main.rs
@@ -1,3 +1,33 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize)]
+pub enum BankAccountCommand {
+    OpenAccount { account_id: String },
+    DepositMoney { amount: f64 },
+    WithdrawMoney { amount: f64 },
+    WriteCheck { check_number: String, amount: f64 },
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub enum BankAccountEvent {
+    AccountOpened {
+        account_id: String,
+    },
+    CustomerDepositedMoney {
+        amount: f64,
+        balance: f64,
+    },
+    CustomerWithdrewCash {
+        amount: f64,
+        balance: f64,
+    },
+    CustomerWroteCheck {
+        check_number: String,
+        amount: f64,
+        balance: f64,
+    },
+}
+
 fn main() {
     println!("Hello, world!");
 }
