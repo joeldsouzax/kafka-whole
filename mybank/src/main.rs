a9cc522eaac3e0662e20346e5a78d4b1c4564714
Joel DSouza <joeldsouzax@gmail.com>
2024-12-31 12:41:42 +0100

    feat(mybank): impl DomainEvent for events

--- mybank/src/main.rs
+++ mybank/src/main.rs
@@ -1,3 +1,4 @@
+use cqrs_es::DomainEvent;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize)]
@@ -26,6 +27,39 @@ pub enum BankAccountEvent {
         amount: f64,
         balance: f64,
     },
+}
+
+impl DomainEvent for BankAccountEvent {
+    fn event_type(&self) -> String {
+        let event_type: &str = match self {
+            BankAccountEvent::AccountOpened { .. } => "AccountOpened",
+            BankAccountEvent::CustomerDepositedMoney { .. } => "CustomerDepositedMoney",
+            BankAccountEvent::CustomerWithdrewCash { .. } => "CustomerWithdrewCash",
+            BankAccountEvent::CustomerWroteCheck { .. } => "CustomerWroteCheck",
+        };
+        event_type.to_string()
+    }
+
+    fn event_version(&self) -> String {
+        "1.0".to_string()
+    }
+}
+
+#[derive(Debug)]
+pub struct BankAccountError(String);
+
+impl std::fmt::Display for BankAccountError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl std::error::Error for BankAccountError {}
+
+impl From<&str> for BankAccountError {
+    fn from(message: &str) -> Self {
+        BankAccountError(message.to_string())
+    }
 }
 
 fn main() {
